473cbe33f77de75ba2a35d9125e9ade8c1ac0389
Sawashi <kietle234.cc@gmail.com>
2022-12-12 23:17:21 +0700

    Basic structure for nodejs + express + handlebars

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 import express from "express";
+import { engine } from "express-handlebars";
 
 const app = express();
 
-const port = 3000;
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-
-app.get('/', (req, res) => {
-	res.send('Hi, hiep still exists.');
-})
-
-
-app.listen(port, () => {
-	console.log(`app is running on localhost:${port}`);
-})
-
-
-
+app.listen(3000);
