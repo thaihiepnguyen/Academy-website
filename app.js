b677c5758113747251e687cee967cef27c891546
Sawashi <kietle234.cc@gmail.com>
2022-12-14 08:27:04 +0700

    Add knex, reconstruct according to url, change extension from handlebars to hbs

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import hbs_sections from "express-handlebars-sections";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import numeral from "numeral";
 
+import productUserRoute from "./routes/product-user.route.js";
+//setup
 const app = express();
+app.use("/public", express.static("public"));
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    helpers: {
+      section: hbs_sections(),
+      format_number(val) {
+        return numeral(val).format("0,0");
+      },
+    },
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.use("/products", productUserRoute);
 
 const PORT = 3000;
 app.listen(PORT, function () {
---
+++ views/vwProduct/detail.hbs
@@ -0,0 +1,1 @@
+<h1>This is detail page</h1>
