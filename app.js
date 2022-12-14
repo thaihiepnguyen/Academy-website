ec745b60c67c1364aa926429c9671beba65f22c7
DungBui <bdung0855@gmail.com>
2022-12-14 19:59:35 +0700

    link view console login

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-import express from "express";
+import express from 'express';
+import { engine } from 'express-handlebars';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 const port = 3000;
 
 
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
 	res.send('Hi, hiep still exists.');
 })
 
-
+app.get('/login', function (req, res) {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  res.sendFile(__dirname + '/views/vwlogin/login.html');
+})
 app.listen(port, () => {
 	console.log(`app is running on localhost:${port}`);
 })
