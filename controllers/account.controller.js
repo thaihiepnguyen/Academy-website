e9b52d71df91dca2f4f1e30adf58f4b71d8a8d9b
thaihiep232002 <thaihiep232002@gmail.com>
2022-12-17 23:27:39 +0700

    commit to pull request

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -41,4 +41,6 @@ export default {
             isLoginPage: true,
         });
     }
+
+
 }
--- utils/db.js
+++ utils/db.js
@@ -6,8 +6,7 @@ export default knexObj({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
-    password: "1234",
-    database: "ecdb_c",
-    //password: reallyStrongPwd123
+    database: "academy-db",
+    password: "reallyStrongPwd123"
   },
 });
