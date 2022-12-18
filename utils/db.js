33e9384a10f2ff032832f7b353b51713108ffdec
DungBui <bdung0855@gmail.com>
2022-12-18 09:43:44 +0700

    home profile and content profile

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,12 @@
-import knexObj from "knex";
+import knexObj from 'knex';
 
 export default knexObj({
-  client: "mysql2",
-  connection: {
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    //password: "1234",
-    //database: "ecdb_c",
-    //password: reallyStrongPwd123
-    password: "root",
-    database: "academy-db"
-  },
+    client: 'mysql2',
+    connection: {
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'academy-db'
+    }
 });
