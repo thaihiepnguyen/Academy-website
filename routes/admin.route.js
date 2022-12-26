efa6204f7aeaa627700cd94a7325ef6305e8e2b9
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2022-12-26 17:06:03 +0700

    Done admin: category

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,10 +1,60 @@
 import express from "express";
+import categoryModel from "../services/category.service.js";
+import courseModel from "../services/courses.service.js";
 
 const router = express.Router();
-router.get("/", async function (req, res) {
-	res.render("vwAdmin/admin.hbs", {
-		isDefault: true,
+router.get("/categories", async function (req, res) {
+	const list = await categoryModel.findAll();
+
+	res.render("vwAdmin/vwCategory/index", {
+		activeTagbarLayout: true,
+		categories: list,
 	});
 });
+router.get("/categories/add", function (req, res) {
+	res.render("vwAdmin/vwCategory/add", {
+		activeTagbarLayout: true,
+	});
+});
+router.post("/categories/add", async function (req, res) {
+	const ret = await categoryModel.add(req.body);
+	res.render("vwAdmin/vwCategory/add", {
+		activeTagbarLayout: true,
+	});
+});
+router.get("/categories/edit", async function (req, res) {
+	const id = req.query.id || 0;
+	const category = await categoryModel.findById(id);
 
+	if (category === null) {
+		return res.redirect("/admin/categories");
+	}
+	res.render("vwAdmin/vwCategory/edit", {
+		activeTagbarLayout: true,
+		category,
+	});
+});
+router.post("/categories/del", async function (req, res) {
+	const id = req.body.id || 0;
+	const courseList = await courseModel.findByCatId(id);
+	req.session.flag = false;
+	if (courseList === null) {
+		const ret = await categoryModel.del(id);
+		return res.redirect("/admin/categories");
+	} else {
+		// res.locals.lcErr_message = "This category has coures, not allowed to be removed.";
+		// console.log("hello world");
+		req.session.err_message = "This category has coures, not allowed to be removed.";
+		req.session.flag = true;
+		res.redirect(req.headers.referer);
+		// res.render("vwAdmin/vwCategory/edit", {
+		// 	activeTagbarLayout: true,
+		// 	err_message: "This category has coures, not allowed to be removed.",
+		// });
+	}
+});
+router.post("/categories/patch", async function (req, res) {
+	const ret = await categoryModel.patch(req.body);
+	res.redirect("/admin/categories");
+});
 export default router;
--- services/category.service.js
+++ services/category.service.js
@@ -1,14 +1,32 @@
-import db from '../utils/db.js';
+import db from "../utils/db.js";
 
 export default {
-    findAll: async () => {
-        const list = await db('categories');
-            // .join('topics', 'topics.field_id', 'categories.id')
-            // .select('topics.id', 'topics.name as t_name', 'categories.name as c_name');
-        if (list.length === 0) {
-            return null;
-        }
+	findAll: async () => {
+		const list = await db("categories");
+		// .join('topics', 'topics.field_id', 'categories.id')
+		// .select('topics.id', 'topics.name as t_name', 'categories.name as c_name');
+		if (list.length === 0) {
+			return null;
+		}
 
-        return list;
-    },
-}
+		return list;
+	},
+	async findById(id) {
+		const list = await db("categories").where("id", id);
+		if (list.length === 0) {
+			return null;
+		}
+		return list[0];
+	},
+	add(entity) {
+		return db("categories").insert(entity);
+	},
+	del(id) {
+		return db("categories").where("id", id).del();
+	},
+	patch(entity) {
+		const id = entity.id;
+		delete entity.id;
+		return db("categories").where("id", id).update(entity);
+	},
+};
