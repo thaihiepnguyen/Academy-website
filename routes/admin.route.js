ed6be141b37279b7b09257eaea5ecb729b3ed519
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-01-01 11:55:51 +0700

    Backup for conflict

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,8 +1,12 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import categoryModel from "../services/category.service.js";
 import courseModel from "../services/courses.service.js";
+import userModel from "../services/user.service.js";
 
 const router = express.Router();
+//=================================================MANAGE CATEGORY=================================================
+//=================================================================================================================
 router.get("/categories", async function (req, res) {
 	const list = await categoryModel.findAll();
 
@@ -42,19 +46,74 @@ router.post("/categories/del", async function (req, res) {
 		const ret = await categoryModel.del(id);
 		return res.redirect("/admin/categories");
 	} else {
-		// res.locals.lcErr_message = "This category has coures, not allowed to be removed.";
-		// console.log("hello world");
 		req.session.err_message = "This category has coures, not allowed to be removed.";
 		req.session.flag = true;
 		res.redirect(req.headers.referer);
-		// res.render("vwAdmin/vwCategory/edit", {
-		// 	activeTagbarLayout: true,
-		// 	err_message: "This category has coures, not allowed to be removed.",
-		// });
 	}
 });
 router.post("/categories/patch", async function (req, res) {
 	const ret = await categoryModel.patch(req.body);
 	res.redirect("/admin/categories");
+});
+//=================================================MANAGE COURSE=================================================
+//===============================================================================================================
+router.get("/courses", async function (req, res) {
+	const list = await courseModel.findAll();
+	res.render("vwAdmin/vwCourse/index", {
+		activeTagbarLayout: true,
+		courses: list,
+	});
+});
+router.post("/courses/del", async function (req, res) {
+	const ret = await courseModel.del(+req.body.id);
+	res.redirect(req.headers.referer);
+});
+//=================================================MANAGE USER===================================================
+//===============================================================================================================
+router.get("/users", async function (req, res) {
+	res.render("vwAdmin/vwUser/index", {
+		activeTagbarLayout: true,
+	});
+});
+router.get("/users/add", function (req, res) {
+	res.render("vwAdmin/vwUser/add", {
+		activeTagbarLayout: true,
+	});
+});
+router.post("/users/add", async function (req, res) {
+	//const ret = await userModel.add(req.body);
+	const rawPassword = req.body.password;
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(rawPassword, salt);
+	req.body.password = hash;
+
+	const user = {
+		...req.body,
+		image: null,
+		role_id: 2,
+	};
+	const entity = await userModel.findAll();
+	let isEmailExists = false;
+	for (let item of entity) {
+		if (item.email === user.email) {
+			isEmailExists = true;
+		}
+	}
+
+	if (!isEmailExists) {
+		await userModel.add(user);
+		res.render("vwAdmin/vwUser/add", {
+			activeTagbarLayout: true,
+		});
+	} else {
+		// res.render("vwSignup/signup", {
+		// 	message: "Email is existed",
+		// 	isDefault: true,
+		// });
+
+		req.session.err_message = "Email is existed.";
+		req.session.flag = true;
+		res.redirect(req.headers.referer);
+	}
 });
 export default router;
--- services/user.service.js
+++ services/user.service.js
@@ -1,68 +1,69 @@
-import db from '../utils/db.js';
+import db from "../utils/db.js";
 
 export default {
-    findAll: async () => {
-        const list = await db('users');
-        if (list.length === 0) {
-            return null;
-        }
+	async findAllWithRole() {
+		const sql = `select u.*, (r.name) as role
+                    from users u left join role r on u.role_id = r.id
+                     group by u.id, u.email`;
+		const raw = await db.raw(sql);
+		return raw[0];
+	},
+	findAll: async () => {
+		const list = await db("users");
+		if (list.length === 0) {
+			return null;
+		}
 
-        return list;
-    },
+		return list;
+	},
 
-    findById: async (id) => {
-        const list = await db('users')
-            .where('id', id);
-        if (list.length === 0) {
-            return null;
-        }
-        return list[0];
-    },
+	findById: async (id) => {
+		const list = await db("users").where("id", id);
+		if (list.length === 0) {
+			return null;
+		}
+		return list[0];
+	},
 
-    findByEmail: async (email) => {
-        const list = await db('users')
-            .where('email', email);
-        if (list.length === 0) {
-            return null;
-        }
-        return list[0];
-    },
+	findByEmail: async (email) => {
+		const list = await db("users").where("email", email);
+		if (list.length === 0) {
+			return null;
+		}
+		return list[0];
+	},
 
-    add: (entity) => {
-        return db('users').insert(entity);
-    },
+	add: (entity) => {
+		return db("users").insert(entity);
+	},
 
-    del: (id) => {
-        return db('users').where('id', id).del();
-    },
+	del: (id) => {
+		return db("users").where("id", id).del();
+	},
 
-    patch: (entity) => {
-        const id = entity.id;
-        //delete entity.id;
-        return db('users').where('id', id).update(entity);
-    },
+	patch: (entity) => {
+		const id = entity.id;
+		//delete entity.id;
+		return db("users").where("id", id).update(entity);
+	},
 
-    findWatchList: async (id) => {
-        const list = await db('watch_list')
-            .join('courses','course_id','id')
-            .where('user_id', id);
+	findWatchList: async (id) => {
+		const list = await db("watch_list").join("courses", "course_id", "id").where("user_id", id);
 
-        if (list.length === 0) {
-            return null;
-        }
+		if (list.length === 0) {
+			return null;
+		}
 
-        return list;
-    },
+		return list;
+	},
 
-    findRegisteredCourses: async (id) => {
-        const list = await db('registered_courses')
-            .join('courses','course_id','id')
-            .where('user_id', id);
+	findRegisteredCourses: async (id) => {
+		const list = await db("registered_courses").join("courses", "course_id", "id").where("user_id", id);
 
-        if (list.length === 0) {
-            return null;
-        }
+		if (list.length === 0) {
+			return null;
+		}
 
-        return list;
-    }
-}
+		return list;
+	},
+};
