5f61553b4a9f7969939d3868b3ce23aadfced301
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-01-01 12:53:36 +0700

    Import Model

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,13 @@
 import knexObj from "knex";
 
 export default knexObj({
-  client: "mysql2",
-  connection: {
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    database: "academy-db",
-    password: "reallyStrongPwd123",
-    //reallyStrongPwd123
-  },
+	client: "mysql2",
+	connection: {
+		host: "127.0.0.1",
+		port: 3306,
+		user: "root",
+		database: "academy-db",
+		password: "root",
+		//reallyStrongPwd123
+	},
 });
